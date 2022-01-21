36bdba8bd5c3e9f13693e84f2c23017294d662a6
cnelsonw <cnelsonw7@gmail.com>
2022-01-20 22:52:34 -0500

    message

--- index.js
+++ index.js
@@ -145,8 +145,13 @@ Lambdasian.prototype.speak = () => {
         + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
         + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
 */
-class Instructor {
-
+class Instructor extends Lambdasian{
+  constructor (obj2) {
+    super(obj2, obj2.name, obj2.age, obj2.location)
+    this.specialty = obj2.specialty
+    this.favLanguage = obj2.favLanguage
+    this.catchPhrase = obj2.catchPhrase
+  }
 }
 /*
   TASK 5
