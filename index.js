86653fbd3d02e567c7b4092f300e84654fd24a27
cnelsonw <cnelsonw7@gmail.com>
2022-01-20 19:00:17 -0500

    message

--- index.js
+++ index.js
@@ -42,7 +42,27 @@ class Airplane {
 */
 
 class Person {
-  
+  constructor (name, age) {
+    this.name = name,
+    this.age = age
+    this.stomach = []
+  }
+
+  eat = (someFood) => {
+    if (this.stomach.length < 10) {
+      this.stomach.push(someFood)
+    }
+  }
+
+  poop = () => {
+    for (let i = -(this.stomach.length); i < 0; i++) {
+      this.stomach.pop() 
+    }
+  }
+
+  toString = () => {
+    return `${this.name}, ${this.age}`
+  }
 }
 
 /*
@@ -60,7 +80,28 @@ class Person {
 */
 
 class Car {
-  
+  constructor (model, milesPerGallon) {
+    this.model = model
+    this.milesPerGallon = milesPerGallon
+    this.tank = 0
+    this.odometer = 0
+  }
+}
+
+Car.prototype.fill = function(gallons) {
+  this.tank += gallons
+}
+
+Car.prototype.drive = function (distance) {
+  if (distance <= this.milesPerGallon * this.tank) {
+  this.odometer += distance
+  this.tank -= distance/this.milesPerGallon
+} else {
+  this.odometer = this.milesPerGallon * this.tank
+  this.tank = 0
+  return `I ran out of fuel at ${this.odometer} miles!`
+}
+
 }
 
 /*
